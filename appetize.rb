b0b62be3e5c0294a0a3a06f6e99539a197c8d41e
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2018-06-03 15:20:43 +0900

    v1.1.1

--- appetize.rb
+++ appetize.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Appetize < Formula
   homepage "https://github.com/YusukeIwaki/appetize-cli"
-  url "https://github.com/YusukeIwaki/appetize-cli/releases/download/v1.1.0/appetize_darwin_amd64"
-  sha256 "c2c23e0bf69885ed65245393e2b516bbd9c19444c0c6a5e5cfccfa52051c3be6"
+  url "https://github.com/YusukeIwaki/appetize-cli/releases/download/v1.1.1/appetize_darwin_amd64"
+  sha256 "12875d28aaf8c00dbfcb5e5c54eace9b09e79bd324344b6e52b8c804293493f2"
   head "https://github.com/YusukeIwaki/appetize-cli.git"
-  version "v1.1.0"
+  version "v1.1.1"
 
   def install
     mv "appetize_darwin_amd64", "appetize"
